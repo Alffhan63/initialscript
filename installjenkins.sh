2dadae62abb2880e69fa48bd67607fe56d33133a
Funcloud6 <alfhan.fauzan@digdayatech.id>
2025-01-02 11:16:31 +0700

    add jenkins

--- installjenkins.sh
+++ installjenkins.sh
@@ -9,12 +9,13 @@ sudo apt-get install curl
 sudo apt install openjdk-11-jdk -y
 
 #GPG key Jenkins
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
-
-
-sudo apt install jenkins -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
